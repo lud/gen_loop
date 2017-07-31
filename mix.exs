0f3de56c6acb4f6dc9b023f17643a978d07dfd51
niahoo <dev@ooha.in>
2017-07-31 15:25:15 +0200

    Added package to mix.exs for Hex publication

--- mix.exs
+++ mix.exs
@@ -3,10 +3,16 @@ defmodule GenLoop.Mixfile do
 
   def project do
     [app: :gen_loop,
+     description: """
+     A supervised free-form loop function. Elixir adapter for plain_fsm, with
+     receive / sync-call macros and GenServer-like starting, stopping and name
+     registration.
+     """,
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     name:" GenLoop",
      deps: deps()]
   end
 
@@ -22,6 +28,14 @@ defmodule GenLoop.Mixfile do
     [
       {:plain_fsm, github: "uwiger/plain_fsm", commit: "ae9eca8a8df8f61a32185b06882a55d60e62e904"},
       {:ex_doc, "~> 0.14", only: :dev},
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      maintainers: ["niahoo osef <dev@ooha.in>"],
+      links: %{"Github" => "https://github.com/niahoo/gen_loop"},
     ]
   end
 end
